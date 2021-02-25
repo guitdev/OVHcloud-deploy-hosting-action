00014e7758ca7b2d97bbe9f255a392126b489867
Guit <ggojard@gmail.com>
2021-02-25 10:53:45 +0100

    add curl hosting url

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3.12
 
 COPY entrypoint.sh /entrypoint.sh
 
-RUN apk --update add --no-cache bash openssh-client sshpass \
+RUN apk --update add --no-cache bash openssh-client sshpass curl \
   && chmod +x /entrypoint.sh && rm -rf /var/cache/apk/*
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Check if needed env vars are available
-if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN || -z $DOCUMENT_ROOT || -z $REPOSITORY_NAME || -z $REPOSITORY_URL ]]; then
+if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_URL || -z $OVH_HOSTING_DOMAIN || -z $DOCUMENT_ROOT || -z $REPOSITORY_NAME || -z $REPOSITORY_URL ]]; then
   echo 'One or more needed env vars are undefined! Take a look at the example usage declaration.'
   exit 1
 fi
+
+curl -L --insecure $OVH_HOSTING_URL
 
 sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "rm -rvf $DOCUMENT_ROOT && mkdir $DOCUMENT_ROOT && git clone $REPOSITORY_URL $DOCUMENT_ROOT"
 
